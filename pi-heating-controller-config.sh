b4dffa2e403d8277da0b3a9ba5ede46e8f6bfb1b
Jeff <Jffrypwll@googlemail.com>
2016-11-21 23:55:00 +0000

    Update pi-heating-controller-config.sh

--- pi-heating-controller-config.sh
+++ pi-heating-controller-config.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Raspberry Pi setup, pi-heating-remote configuration script.
+# Author: Jeffrey.Powell ( jffrypwll <at> googlemail <dot> com )
+
+# Die on any errors
+set -e 
+
+if [[ `whoami` != "root" ]]
+then
+  echo "Script must be run as root."
+  exit 1
+fi
