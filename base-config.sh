c3c469ec93467271a200ecc5b7af34e8a974c973
Jeff <Jffrypwll@googlemail.com>
2016-11-23 11:35:05 +0000

    Update base-config.sh

--- base-config.sh
+++ base-config.sh
@@ -37,6 +37,27 @@ printf "\n\n Current IP address is : $OLD_IPADDR\n"
 printf " Please choose a new STATIC IP address: (blank to skip) "
 read NEW_IPADDR
 
+cat > /etc/network/interfaces <<INTERFACES
+# interfaces(5) file used by ifup(8) and ifdown(8)
+
+# Please note that this file is written to be used with dhcpcd
+# For static IP, consult /etc/dhcpcd.conf and 'man dhcpcd.conf'
+
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+iface eth0 inet manual
+
+allow-hotplug wlan0
+iface wlan0 inet manual
+    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+INTERFACES
+
+
+
 printf "\n\n Configuring aliases ... \n"
 cat > /home/pi/.bash_aliases <<ALIASES
 alias ll='ls -l'
