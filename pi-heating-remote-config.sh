b51fe25b31ff0b1d7262b8ea4c622042342690e0
Jeff <Jffrypwll@googlemail.com>
2016-11-24 15:16:11 +0000

    Update pi-heating-remote-config.sh

--- pi-heating-remote-config.sh
+++ pi-heating-remote-config.sh
@@ -57,9 +57,9 @@ then
   #tar -xvzf "$PI_HEATING_V.tar.gz"
   #chown pi:pi "pi-heating-remote-$PI_HEATING_V"
   #mv -R "pi-heating-remote-$PI_HEATING_V" "pi-heating-remote"
-  tar -xvzf "pi-heating-remote.tar.gz"
-  mv -R "pi-heating-remote-$PI_HEATING_V" "pi-heating-remote"
-  chown -R pi:pi "pi-heating-remote"
+  tar -xvzf "/home/pi/pi-heating-remote.tar.gz"
+  mv -R "/home/pi/pi-heating-remote-$PI_HEATING_V" "/home/pi/pi-heating-remote"
+  chown -R pi:pi "/home/pi/pi-heating-remote"
   
   if [ ! -f "/home/pi/pi-heating-remote/README.md" ]
     then
