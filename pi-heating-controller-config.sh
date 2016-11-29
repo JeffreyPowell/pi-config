4d3811b9447c5d350b1ef271356b140b10ee7791
Jeff <Jffrypwll@googlemail.com>
2016-11-29 13:52:26 +0000

    Update pi-heating-controller-config.sh

--- pi-heating-controller-config.sh
+++ pi-heating-controller-config.sh
@@ -1,13 +1,152 @@
 #!/bin/bash
 
-# Raspberry Pi setup, pi-heating-remote configuration script.
-# Author: Jeffrey.Powell ( jffrypwll <at> googlemail <dot> com )
+#          Raspberry Pi setup, 'pi-heating-hub' configuration script.
+# Author : Jeffrey.Powell ( jffrypwll <at> googlemail <dot> com )
+# Date   : Nov 2016
 
 # Die on any errors
-set -e 
+
+#set -e 
+clear
 
 if [[ `whoami` != "root" ]]
 then
-  echo "Script must be run as root."
+  printf "\n\n Script must be run as root. \n\n"
   exit 1
 fi
+
+OS_VERSION=$(cat /etc/os-release | grep VERSION=)
+if [[ $OS_VERSION != *"jessie"* ]]
+then
+  printf "\n\n EXITING : Script must be run on PI OS Jessie. \n\n"
+  exit 1
+fi
+
+APACHE_INSTALLED=$(which apache2)
+if [[ "$APACHE_INSTALLED" == "" ]]
+then
+  printf "\n\n Installing Apache ...\n"
+  # Install Apache
+  apt-get install apache2 -y
+  update-rc.d apache2 enable
+  a2dissite 000-default.conf
+  service apache2 restart
+  
+  APACHE_INSTALLED=$(which apache2)
+    if [[ "$APACHE_INSTALLED" == "" ]]
+    then
+      printf "\n\n EXITING : Apache installation FAILED\n"
+      exit 1
+    fi
+else
+  printf "\n\n Apache is already installed. \n"
+fi
+
+PHP_INSTALLED=$(which php)
+echo ">$PHP_INSTALLED<"
+if [[ "$PHP_INSTALLED" == "" ]]
+then
+  printf "\n\n Installing PHP ...\n"
+  # Install Apache
+  apt-get install php5 -y
+  
+  PHP_INSTALLED=$(which php)
+    if [[ "$PHP_INSTALLED" == "" ]]
+    then
+      printf "\n\n EXITING : PHP installation FAILED\n"
+      exit 1
+    fi
+else
+  printf "\n\n PHP is already installed. \n"
+fi
+
+MYSQL_INSTALLED=$(which mysql)
+echo ">$MYSQL_INSTALLED<"
+if [[ "$MYSQL_INSTALLED" == "" ]]
+then
+  printf "\n\n Installing MYSQL ...\n"
+  # Install Apache
+  apt-get install mysql-server -y --fix-missing
+  
+  MYSQL_INSTALLED=$(which mysql)
+    if [[ "$MYSQL_INSTALLED" == "" ]]
+    then
+      printf "\n\n EXITING : MYSQL installation FAILED\n"
+      exit 1
+    fi
+else
+  printf "\n\n MYSQL is already installed. \n"
+fi
+
+
+# Install 'pi-heating-hub' app
+
+PI_HEATING_V='0.0.1'
+if [ ! -f "/home/pi/pi-heating-hub/README.md" ]
+then
+  printf "\n\n Installing pi-heating-hub v$PI_HEATING_V ...\n"
+  # Install Apache
+  cd /home/pi
+  if [ -d "/home/pi/pi-heating-hub" ]
+  then
+    rm -rf "/home/pi/pi-heating-hub"
+  fi
+  
+  if [ -d "/var/www/pi-heating-hub" ]
+  then
+    rm -rf "/var/www/pi-heating-hub"
+  fi
+  
+  wget "https://github.com/JeffreyPowell/pi-heating-hub/archive/$PI_HEATING_V.tar.gz" -O "/home/pi/pi-heating-hub.tar.gz"
+  tar -xvzf "/home/pi/pi-heating-hub.tar.gz"
+  rm "/home/pi/pi-heating-hub.tar.gz"
+  mv "/home/pi/pi-heating-hub-$PI_HEATING_V" "/home/pi/pi-heating-hub"
+  mv "/home/pi/pi-heating-hub/www" "/var/www/pi-heating-hub"
+  chown -R pi:pi "/home/pi/pi-heating-hub"
+  chmod -R 750 "/home/pi/pi-heating-hub"
+  chown -R pi:pi "/var/www/pi-heating-hub"
+  chmod -R 755 "/var/www/pi-heating-hub"
+  
+  if [ ! -f "/home/pi/pi-heating-hub/README.md" ]
+    then
+      printf "\n\n EXITING : pi-heating-hub v$PI_HEATING_V installation FAILED\n"
+      exit 1
+    fi
+    
+else
+  printf "\n\n pi-heating-hub v$PI_HEATING_V is already installed. \n"
+fi
+
+
+
+# configure app
+
+# configure apache vh on port 8080
+
+printf "\n\n Configuring Apache ...\n"
+
+  cat > /etc/apache2/sites-available/pi-heating.conf <<PORTS
+Listen 8080
+PORTS
+
+  cat > /etc/apache2/sites-available/pi-heating.conf <<VHOST
+<VirtualHost *:8080>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/pi-heating-hub/
+    <Directory /var/www/pi-heating-hub/>
+        Options -Indexes
+        AllowOverride all
+        Order allow,deny
+        allow from all
+    </Directory>
+    
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+VHOST
+
+a2ensite pi-heating.conf
+service apache2 restart
+
+printf "\n\n Installation Complete. Some changes might require a reboot. \n\n"
+exit 1
