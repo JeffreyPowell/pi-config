3e6acc07f300f6351a8257fad636cac0376041d1
Jeff <Jffrypwll@googlemail.com>
2016-12-04 16:36:58 +0000

    Update pi-heating-hub-mysql-setup.sh

--- pi-heating-hub-mysql-setup.sh
+++ pi-heating-hub-mysql-setup.sh
@@ -35,37 +35,37 @@ INSERT INTO  modes VALUES ( 1,       'Holiday',         false );
 INSERT INTO  modes VALUES ( 2,       'Weekend Away',    false );
 INSERT INTO  modes VALUES ( 3,       'Guests Staying',  false );
 
-CREATE TABLE schedules        ( id INT,      name VARCHAR(256),          start TIMESTAMP, end TIMESTAMP,   dow INT, value BOOLEAN );
-INSERT INTO  schedules VALUES ( 1,               'Heating Weekday Morning',  '09:00:00', '18:00:00', 124,         false );
-INSERT INTO  schedules VALUES ( 2,               'Heating Weekday Evening',  '17:30:00', '21:00:00', 124,         false );
-INSERT INTO  schedules VALUES ( 3,               'Heating Weekday All day',  '00:01:00', '23:59:00', 124,         false );
-INSERT INTO  schedules VALUES ( 4,               'Heating Everyday Boost',   '00:01:00', '23:59:00', 127,         false );
-INSERT INTO  schedules VALUES ( 5,               'Heating Weekend All day',  '09:00:00', '18:00:00', 3,           false );
-INSERT INTO  schedules VALUES ( 6,               'Water Weekday Morning',    '06:00:00', '08:00:00', 124,         false );
-INSERT INTO  schedules VALUES ( 7,               'Water Weekday Evening',    '16:00:00', '21:00:00', 124,         false );
+CREATE TABLE schedules        ( id INT, name VARCHAR(256),          start TIME, end TIME,   dow INT, value BOOLEAN );
+INSERT INTO  schedules VALUES ( 1,      'Heating Weekday Morning',  '09:00:00', '18:00:00', 124,     false );
+INSERT INTO  schedules VALUES ( 2,      'Heating Weekday Evening',  '17:30:00', '21:00:00', 124,     false );
+INSERT INTO  schedules VALUES ( 3,      'Heating Weekday All day',  '00:01:00', '23:59:00', 124,     false );
+INSERT INTO  schedules VALUES ( 4,      'Heating Everyday Boost',   '00:01:00', '23:59:00', 127,     false );
+INSERT INTO  schedules VALUES ( 5,      'Heating Weekend All day',  '09:00:00', '18:00:00', 3,       false );
+INSERT INTO  schedules VALUES ( 6,      'Water Weekday Morning',    '06:00:00', '08:00:00', 124,     false );
+INSERT INTO  schedules VALUES ( 7,      'Water Weekday Evening',    '16:00:00', '21:00:00', 124,     false );
 
 CREATE TABLE sched_output        ( id INT, sched_id INT, output_id INT );
-INSERT INTO  sched_output VALUES ( 1,          1,               1 );
-INSERT INTO  sched_output VALUES ( 2,          2,               1 );
-INSERT INTO  sched_output VALUES ( 3,          3,               1 );
-INSERT INTO  sched_output VALUES ( 4,          4,               1 );
-INSERT INTO  sched_output VALUES ( 5,          5,               1 );
-INSERT INTO  sched_output VALUES ( 6,          6,               1 );
-INSERT INTO  sched_output VALUES ( 7,          1,               2 );
-INSERT INTO  sched_output VALUES ( 8,          2,               2 );
-INSERT INTO  sched_output VALUES ( 9,          3,               2 );
-INSERT INTO  sched_output VALUES (10,          4,               2 );
+INSERT INTO  sched_output VALUES ( 1,      1,            1 );
+INSERT INTO  sched_output VALUES ( 2,      2,            1 );
+INSERT INTO  sched_output VALUES ( 3,      3,            1 );
+INSERT INTO  sched_output VALUES ( 4,      4,            1 );
+INSERT INTO  sched_output VALUES ( 5,      5,            1 );
+INSERT INTO  sched_output VALUES ( 6,      6,            1 );
+INSERT INTO  sched_output VALUES ( 7,      1,            2 );
+INSERT INTO  sched_output VALUES ( 8,      2,            2 );
+INSERT INTO  sched_output VALUES ( 9,      3,            2 );
+INSERT INTO  sched_output VALUES (10,      4,            2 );
 
 CREATE TABLE sched_input        ( id INT, sched_id INT, input_id INT, opp CHAR, value FLOAT );
-INSERT INTO  sched_input VALUES ( 1,          1,               1,                '<',           18.5 );
-INSERT INTO  sched_input VALUES ( 2,          2,               1,                '<',           20.0 );
-INSERT INTO  sched_input VALUES ( 3,          3,               1,                '<',           10.0 );
+INSERT INTO  sched_input VALUES ( 1,      1,            1,            '<',      18.5 );
+INSERT INTO  sched_input VALUES ( 2,      2,            1,            '<',      20.0 );
+INSERT INTO  sched_input VALUES ( 3,      3,            1,            '<',      10.0 );
 
-CREATE TABLE sched_timers        ( id INT, sched_id INT, timer_id INT, opp CHAR, value BOOLEANT );
-INSERT INTO  sched_timers VALUES ( 1,          1,               1,                '=',           False );
+CREATE TABLE sched_timers        ( id INT, sched_id INT, timer_id INT, opp CHAR, value BOOLEAN );
+INSERT INTO  sched_timers VALUES ( 1,      1,            1,            '=',      False );
 
-CREATE TABLE sched_modes        ( id INT, sched_id INT, mode_id INT, opp CHAR, value BOOLEANT );
-INSERT INTO  sched_modes VALUES ( 1,          1,               1,                '=',           False );
+CREATE TABLE sched_modes         ( id INT, sched_id INT, mode_id INT, opp CHAR, value BOOLEAN );
+INSERT INTO  sched_modes VALUES  ( 1,      1,            1,           '=',      False );
 
 EOF
 
