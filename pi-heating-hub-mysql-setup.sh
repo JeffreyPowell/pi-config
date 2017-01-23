c671a113435dc4df46c75a91f3426ac476135578
Jeff <Jffrypwll@googlemail.com>
2017-01-23 18:41:21 +0000

    Update pi-heating-hub-mysql-setup.sh

--- pi-heating-hub-mysql-setup.sh
+++ pi-heating-hub-mysql-setup.sh
@@ -28,20 +28,34 @@ CREATE TABLE devices        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 INSERT INTO  devices VALUES ( 1,       'Hot Water Boiler',       08,          false );
 INSERT INTO  devices VALUES ( 2,       'Heating Circulation',         10,          false );
 
-CREATE TABLE sensors        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ref VARCHAR(20), name VARCHAR(256), ip  VARCHAR(16), value FLOAT );
+CREATE TABLE sensors        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                              ref VARCHAR(20), 
+                              name VARCHAR(256), 
+                              ip  VARCHAR(16), 
+                              value FLOAT );
 INSERT INTO  sensors VALUES ( 1,      '28-0000000',    'Lounge',          '192.168.0.11',  0.0 );
 INSERT INTO  sensors VALUES ( 2,      '28-0000001',    'Conservatory',    '192.168.0.11',  0.0 );
 
-CREATE TABLE timers        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), duration INT, start TIME, value BOOLEAN );
-INSERT INTO  timers VALUES ( 1,       'Hot Water Boost', 30,           '00:00:00',      false );
-INSERT INTO  timers VALUES ( 2,       'Heating Boost',   30,           '',              false );
+CREATE TABLE timers        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                             name VARCHAR(256), 
+                             duration INT, 
+                             value BOOLEAN );
+INSERT INTO  timers VALUES ( 1,       '+ Hot Water', 30,      false );
+INSERT INTO  timers VALUES ( 2,       '+ Heating',   30,      false );
 
-CREATE TABLE modes        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), value BOOLEAN );
+CREATE TABLE modes        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                            name VARCHAR(256), 
+                            value BOOLEAN );
 INSERT INTO  modes VALUES ( 1,       'Holiday',         false );
 INSERT INTO  modes VALUES ( 2,       'Weekend Away',    false );
 INSERT INTO  modes VALUES ( 3,       'Guests Staying',  false );
 
-CREATE TABLE schedules        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256),          start TIME, end TIME,   dow INT, value BOOLEAN );
+CREATE TABLE schedules        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                name VARCHAR(256),          
+                                start TIME, 
+                                end TIME,   
+                                dow INT, 
+                                value BOOLEAN );
 INSERT INTO  schedules VALUES ( 1,      'Heating Weekday Morning',  '09:00:00', '18:00:00', 124,     false );
 INSERT INTO  schedules VALUES ( 2,      'Heating Weekday Evening',  '17:30:00', '21:00:00', 124,     false );
 INSERT INTO  schedules VALUES ( 3,      'Heating Weekday All day',  '00:01:00', '23:59:00', 124,     false );
@@ -50,7 +64,9 @@ INSERT INTO  schedules VALUES ( 5,      'Heating Weekend All day',  '09:00:00', 
 INSERT INTO  schedules VALUES ( 6,      'Water Weekday Morning',    '06:00:00', '08:00:00', 124,     false );
 INSERT INTO  schedules VALUES ( 7,      'Water Weekday Evening',    '16:00:00', '21:00:00', 124,     false );
 
-CREATE TABLE sched_device        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sched_id INT, device_id INT );
+CREATE TABLE sched_device        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                   sched_id INT, 
+                                   device_id INT );
 INSERT INTO  sched_device VALUES ( 1,      1,            1 );
 INSERT INTO  sched_device VALUES ( 2,      2,            1 );
 INSERT INTO  sched_device VALUES ( 3,      3,            1 );
@@ -62,15 +78,27 @@ INSERT INTO  sched_device VALUES ( 8,      2,            2 );
 INSERT INTO  sched_device VALUES ( 9,      3,            2 );
 INSERT INTO  sched_device VALUES (10,      4,            2 );
 
-CREATE TABLE sched_sensor        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sched_id INT, sensor_id INT, opp CHAR, value FLOAT );
+CREATE TABLE sched_sensor        ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                   sched_id INT, 
+                                   sensor_id INT, 
+                                   opp CHAR, 
+                                   value FLOAT );
 INSERT INTO  sched_sensor VALUES ( 1,      1,            1,            '<',      18.5 );
 INSERT INTO  sched_sensor VALUES ( 2,      2,            1,            '<',      20.0 );
 INSERT INTO  sched_sensor VALUES ( 3,      3,            1,            '<',      10.0 );
 
-CREATE TABLE sched_timer         ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sched_id INT, timer_id INT, opp CHAR, value BOOLEAN );
+CREATE TABLE sched_timer         ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                   sched_id INT, 
+                                   timer_id INT, 
+                                   opp CHAR, 
+                                   value BOOLEAN );
 INSERT INTO  sched_timers VALUES ( 1,      1,            1,            '=',      False );
 
-CREATE TABLE sched_mode          ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sched_id INT, mode_id INT, opp CHAR, value BOOLEAN );
+CREATE TABLE sched_mode          ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                   sched_id INT, 
+                                   mode_id INT, 
+                                   opp CHAR, 
+                                   value BOOLEAN );
 INSERT INTO  sched_modes VALUES  ( 1,      1,            1,           '=',      False );
 
 DATABASE
