fffd97856e915bed67d5439e6c9a26528b3b548e
Jeff <Jffrypwll@googlemail.com>
2017-01-25 23:19:31 +0000

    Update pi-heating-hub-mysql-setup.sh

--- pi-heating-hub-mysql-setup.sh
+++ pi-heating-hub-mysql-setup.sh
@@ -22,79 +22,79 @@ FLUSH PRIVILEGES;
 USE pi_heating_db;
 
 CREATE TABLE IF NOT EXISTS 'devices'   (
-                                            `d_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `name` varchar(256) NOT NULL,
-                                            `pin` int(11) DEFAULT NULL,
-                                            `active_level` tinyint(4) DEFAULT NULL,
-                                            `value` tinyint(1) DEFAULT '0'
+                                            'd_id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'name' varchar(256) NOT NULL,
+                                            'pin' int(11) DEFAULT NULL,
+                                            'active_level' tinyint(4) DEFAULT NULL,
+                                            'value' tinyint(1) DEFAULT '0'
                                             );
   
-CREATE TABLE IF NOT EXISTS `sensors`   (
-                                            `id` bigint(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `ref` varchar(20) DEFAULT NULL,
-                                            `name` varchar(256) DEFAULT NULL,
-                                            `ip` varchar(16) DEFAULT NULL,
-                                            `value` float DEFAULT NULL,
-                                            `unit` varchar(11) NOT NULL
+CREATE TABLE IF NOT EXISTS 'sensors'   (
+                                            'id' bigint(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'ref' varchar(20) DEFAULT NULL,
+                                            'name' varchar(256) DEFAULT NULL,
+                                            'ip' varchar(16) DEFAULT NULL,
+                                            'value' float DEFAULT NULL,
+                                            'unit' varchar(11) NOT NULL
                                             );
 
-CREATE TABLE IF NOT EXISTS `timers`    (
-                                            `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `name` varchar(256) DEFAULT NULL,
-                                            `duration` int(11) DEFAULT NULL,
-                                            `value` tinyint(1) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'timers'    (
+                                            'id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'name' varchar(256) DEFAULT NULL,
+                                            'duration' int(11) DEFAULT NULL,
+                                            'value' tinyint(1) DEFAULT NULL
                                             ); 
 
-CREATE TABLE IF NOT EXISTS `modes`     (
-                                            `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `name` varchar(256) DEFAULT NULL,
-                                            `value` tinyint(1) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'modes'     (
+                                            'id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'name' varchar(256) DEFAULT NULL,
+                                            'value' tinyint(1) DEFAULT NULL
                                             );
                             
-CREATE TABLE IF NOT EXISTS `schedules` (
-                                            `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-                                            `name` varchar(256) DEFAULT NULL,
-                                            `start` time DEFAULT NULL,
-                                            `end` time DEFAULT NULL,  
-                                            `dow1` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow2` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow3` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow4` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow5` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow6` tinyint(1) NOT NULL DEFAULT '0',
-                                            `dow7` tinyint(1) NOT NULL DEFAULT '0',
-                                            `enabled` tinyint(1) NOT NULL DEFAULT '1',
-                                            `active` tinyint(1) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'schedules' (
+                                            'id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                                            'name' varchar(256) DEFAULT NULL,
+                                            'start' time DEFAULT NULL,
+                                            'end' time DEFAULT NULL,  
+                                            'dow1' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow2' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow3' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow4' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow5' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow6' tinyint(1) NOT NULL DEFAULT '0',
+                                            'dow7' tinyint(1) NOT NULL DEFAULT '0',
+                                            'enabled' tinyint(1) NOT NULL DEFAULT '1',
+                                            'active' tinyint(1) DEFAULT NULL
                                             );
 
-CREATE TABLE IF NOT EXISTS `sched_device` (
-                                            `sd_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `sched_id` int(11) DEFAULT NULL,
-                                            `device_id` int(11) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'sched_device' (
+                                            'sd_id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'sched_id' int(11) DEFAULT NULL,
+                                            'device_id' int(11) DEFAULT NULL
                                             );
 
-CREATE TABLE IF NOT EXISTS `sched_sensor` (
-                                            `ss_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `sched_id` int(11) DEFAULT NULL,
-                                            `sensor_id` int(11) DEFAULT NULL,
-                                            `opp` char(1) DEFAULT NULL,
-                                            `value` float DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'sched_sensor' (
+                                            'ss_id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'sched_id' int(11) DEFAULT NULL,
+                                            'sensor_id' int(11) DEFAULT NULL,
+                                            'opp' char(1) DEFAULT NULL,
+                                            'value' float DEFAULT NULL
                                             );
 
-CREATE TABLE IF NOT EXISTS `sched_timer` (
-                                            `st_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `sched_id` int(11) DEFAULT NULL,
-                                            `timer_id` int(11) DEFAULT NULL,
-                                            `opp` char(1) DEFAULT NULL,
-                                            `value` tinyint(1) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'sched_timer' (
+                                            'st_id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'sched_id' int(11) DEFAULT NULL,
+                                            'timer_id' int(11) DEFAULT NULL,
+                                            'opp' char(1) DEFAULT NULL,
+                                            'value' tinyint(1) DEFAULT NULL
                                             );
 
-CREATE TABLE IF NOT EXISTS `sched_mode` (
-                                            `sm_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            `sched_id` int(11) DEFAULT NULL,
-                                            `mode_id` int(11) DEFAULT NULL,
-                                            `test_opp` char(1) DEFAULT NULL,
-                                            `test_value` tinyint(1) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS 'sched_mode' (
+                                            'sm_id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                            'sched_id' int(11) DEFAULT NULL,
+                                            'mode_id' int(11) DEFAULT NULL,
+                                            'test_opp' char(1) DEFAULT NULL,
+                                            'test_value' tinyint(1) DEFAULT NULL
                                             );
 DATABASE
 
